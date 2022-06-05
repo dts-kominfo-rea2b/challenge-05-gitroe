c8770b846e178d78d6713b5f260caf1d0f7daf3f
gitroe <aingtehheru@outlook.com>
2022-06-05 18:58:58 +0700

    update challenge Callback
    mengupdate TODO sesuai tantangan

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (daftarNama, tipeUrutan) => {  
+  const hasilTanpaNomor= tipeUrutan(daftarNama);
+  const hasilDenganNomor= [];
+  for (
+    let data = 0;
+    data < hasilTanpaNomor.length;
+    data++
+    ) {
+    hasilDenganNomor.push(`${data + 1}. ${hasilTanpaNomor[data]}`);
+  }
+  return hasilDenganNomor;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (daftarNama) => {
+  return daftarNama.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (daftarNama) => {
+  return daftarNama.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
